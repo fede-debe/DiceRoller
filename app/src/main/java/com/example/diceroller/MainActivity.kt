1577c69cb1843ea129cf37a532a05073278684da
Federico De Benedictis <66374898+MenneaDb@users.noreply.github.com>
2021-03-02 21:36:46 +0100

    the app now can display the image of the dice with the number related to the result of the method that get executed when the user click the button.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,21 +12,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btn_roll.text = resources.getString(R.string.roll) // change the value of the text by using a String
 
-        btn_roll.text = resources.getString(R.string.roll)
-
+        // set an event for this View
         btn_roll.setOnClickListener{
             // Toast.makeText(this, "Let's Roll", Toast.LENGTH_LONG).show()
-            rollDice()
+            rollDice() // each time the user click on it will get a result from this method
         }
-       
-
     }
 
     private fun rollDice() {
-        val randomInt = (1..6).random()
-        tv_roll_result.text = randomInt.toString()
+        val resultImageDice = when ((1..6).random()) { // based on the random result I want to display the related image
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            else-> R.drawable.dice_6
+        }
+
+        dice_image.setImageResource(resultImageDice) // we set the image related to the random number to this View
     }
-
-
 }
